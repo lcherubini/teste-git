2c5a368bdfbfa7b6f2c1c70ae0a85c000a7e3135
leocherubini <cherubini18@hotmail.com>
2014-07-06 18:54:28 -0400

    git funcionando aqui

--- TesteGit/src/com/teste/git/TesteGit.java
+++ TesteGit/src/com/teste/git/TesteGit.java
@@ -3,7 +3,8 @@ package com.teste.git;
 public class TesteGit {
 
 	public static void main(String[] args) {
-		System.out.println("Git funcionando...");
+		System.out.println("Git funcionando aqui também...");
+		
 	}
 
 }
