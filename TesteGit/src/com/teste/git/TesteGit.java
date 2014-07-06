40dde7d01c8693238f7c2ebb3065c19b6d692bb5
leocherubini <cherubini18@hotmail.com>
2014-07-06 18:57:28 -0400

    commit diferente

--- TesteGit/src/com/teste/git/TesteGit.java
+++ TesteGit/src/com/teste/git/TesteGit.java
@@ -4,7 +4,7 @@ public class TesteGit {
 
 	public static void main(String[] args) {
 		System.out.println("Git funcionando aqui também...");
-		
+		System.out.println("Um commit diferente aqui");
 	}
 
 }
